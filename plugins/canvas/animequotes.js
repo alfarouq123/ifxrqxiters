4d61d332e90f4d2464e7e5d4532f809141a2e2cc
alfarouq123 <112934598+alfarouq123@users.noreply.github.com>
2026-08-28 17:45:32 +0800

    Delete plugins directory

--- plugins/canvas/animequotes.js
+++
@@ -1,7 +0,0 @@
-const { makeSimpleImagePlugin } = require('../../lib/engine/imageApi');
-module.exports = makeSimpleImagePlugin({
-  name: 'animequotes', alias: ['animequote'],
-  description: 'Membuat gambar quotes dengan gaya aesthetic anime',
-  usage: '/animequotes <teks quote>', example: '/animequotes Jangan menyerah, terus melangkah',
-  buildUrl: (t) => `https://api.cuki.biz.id/api/canvas/animequotes?apikey=cuki&text=${encodeURIComponent(t)}`,
-});
--- plugins/canvas/balogo.js
+++
@@ -1,7 +0,0 @@
-const { makeSimpleImagePlugin } = require('../../lib/engine/imageApi');
-module.exports = makeSimpleImagePlugin({
-  name: 'balogo', alias: ['bluearchivelogo'],
-  description: 'Membuat logo teks bergaya Blue Archive',
-  usage: '/balogo <teks>', example: '/balogo IFxrq',
-  buildUrl: (t) => `https://api.nexray.web.id/maker/balogo?text=${encodeURIComponent(t)}`,
-});
